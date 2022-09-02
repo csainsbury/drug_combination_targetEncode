041300d52bfd1095af6ee6b051e1d9e891764c82
Chris Sainsbury <csainsbury@mba.local>
2022-09-02 15:38:36 +0100

    add weight plotting folder

---
+++ weight_plot/weight_plot.R
@@ -0,0 +1,13 @@
+library(data.table)
+x <- fread('~/Downloads/BMI_weight_trajectory - current.csv', stringsAsFactors = F)
+x$V1 <- as.Date(x$V1, format = '%d/%m/%Y')
+
+plot(x$V1, x$weight)
+
+x = x[70:nrow(x), ]
+x$days <- c(1:nrow(x))
+
+plot(x$days, x$weight)
+
+abline(lm(x$weight ~ x$days))
+print(lm(x$weight ~ x$days))
